bb6364d78b7d87d5d52def67508a663d784590da
CWYH <cwyhxq@gmail.com>
2019-12-24 22:47:57 +0800

    240. Search a 2D Matrix II -- Microsoft

--- BinarySearch/BinaySearch.md
+++ BinarySearch/BinaySearch.md
@@ -6,3 +6,7 @@
 
 #### 475. Heaters -- Easy
 对于每一个house， 在heaters中二分查找它的位置，找到最近的heater。这样所有最近的heater距离的最大值就是结果。
+
+### Medium
+#### 240. Search a 2D Matrix II -- Microsoft
+从最后一列往前开始二分查找，逐渐逼近。
---
+++ BinarySearch/P0240_Search_a_2D_Matrix/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * 240. Search a 2D Matrix -- Medium
+ * 
+ * Binary Search
+ * 
+ * Microsoft
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        final int M = matrix.length;
+        if (M <= 0) return false;
+        final int N = matrix[0].length;
+        if (N <= 0) return false;
+
+        int j = N - 1;
+        int i = 0;
+        while (j >= 0) {
+            int pos = binarySearch(matrix, j, i, target);
+            if (pos >= M) {
+                return false;
+            }
+            if (matrix[pos][j] == target) {
+                return true;
+            } else {
+                j--;
+                i = pos;
+            }
+        }
+        return false;
+    }
+
+    private int binarySearch(int[][] matrix, int row, int i, int target) {
+        int lo = i;
+        int hi = matrix.length - 1;
+        int mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (target < matrix[mid][row]) {
+                hi = mid - 1;
+            } else if (matrix[mid][row] < target) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return lo;
+    }
+}
