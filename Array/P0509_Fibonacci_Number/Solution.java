3fed2036644364ff6fd7e44753addb17d6dc195d
CWYH <cwyhxq@gmail.com>
2019-12-26 17:39:53 +0800

    509

---
+++ Array/P0509_Fibonacci_Number/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * 509. Fibonaci Number -- Easy
+ */
+
+class Solution {
+    public int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i <= N; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
