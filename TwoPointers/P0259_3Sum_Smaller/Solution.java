a96dee6ad9ccb52aff0635bb8bf808e730c4fddc
Xue Qian <cwyhxq@gmail.com>
2020-01-21 12:01:48 +0800

    259

--- TwoPointers/P0259_3Sum_Smaller/Solution.java
+++ TwoPointers/P0259_3Sum_Smaller/Solution.java
@@ -1,18 +1,7 @@
-# Two Pointers 双指针法
+/**
+ * 259. 3Sum Smaller -- Medium
+ */
 
-## Leetcode
-
-### Easy
-### Medium
-#### 159. Longest Substring with At Most Two Distinct Characters -- Medium
-注意的测试点："ababaaaccc"
-
-#### 259. 3Sum Smaller -- Medium
-时间复杂度$O(n^2)$
-
-3sum转化为2sum，用双指针解决。
-
-```java
 class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
@@ -38,10 +27,3 @@ class Solution {
         return sum;
     }
 }
-```
-
-
-### Hard
-#### 76. Minimum Window String -- Hard
-双指针
-动态维护一个区间。尾指针不断后扫，当扫到有一个窗口包含了所有T的字符后，然后再收缩头指针，直到不能收缩为止。最后记录所有可能的情况中窗口最小的。
